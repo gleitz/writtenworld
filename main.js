291e35e6bb9c02c223075b704996f047badbdb90
Benjamin Gleitzman <gleitz@mit.edu>
2017-11-25 22:05:24 -0800

    Update to use geocoder

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
 /* global $ google document */
 
-const LAT_LNG_FROM_GOOGLE_MAPS_REGEX = /@(\-?[0-9]+\.[0-9]+),(\-?[0-9]+\.[0-9]+)/;
 const GOOGLE_SHEET_ID = '1FnoiLkT1MZ1X5F6mBrAJZJFc9fbp6yfi5rDSeiMKkP8';
 const AUTHOR_KEY = 'gsx$authoroptional';
-const URL_KEY = 'gsx$googlemapslink';
+const ADDRESS_KEY = 'gsx$address';
 const POEM_KEY = 'gsx$poem';
 const TITLE_KEY = 'gsx$poemtitle';
 const DEFAULT_ZOOM_LEVEL = 11;
 
+let geocoder;
+let map;
 let isMapInitialized = false;
+let isMapCentered = false;
 
-function extractLatLngFromGoogleMapsUrl(url) {
-  const matches = LAT_LNG_FROM_GOOGLE_MAPS_REGEX.exec(url);
-  if (!matches || matches.length !== 3) {
-    console.log('no match for url ' + url);
+function addToMap(poem) {
+  if (!isMapCentered) {
+    map.setCenter(poem.location);
+    isMapCentered = true;
   }
-  return { lat: parseFloat(matches[1]),
-           lng: parseFloat(matches[2]) };
+  const marker = new google.maps.Marker({
+    position: poem.location,
+    title: poem.title,
+    map,
+  });
+  const contentHtml = poem.poem.replace(/\n/g, '<br/>');
+  let content = `<b>${poem.title}</b><br/><br/>
+        ${contentHtml}<br/>`;
+  if (poem.author) {
+    content = `${content}<br/>
+      -- ${poem.author}`;
+  }
+  const infoWindow = new google.maps.InfoWindow({
+    content,
+  });
+  marker.addListener('click', () => {
+    let panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('pano'), {
+        position: poem.location,
+        pov: {
+          heading: 34,
+          pitch: 10,
+        },
+      });
+    map.setStreetView(panorama);
+    infoWindow.open(map, marker);
+  });
 }
 
 function loadPoemsIntoMap(poems) {
@@ -25,32 +52,16 @@ function loadPoemsIntoMap(poems) {
       loadPoemsIntoMap(poems);
     }, 1000);
   }
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: DEFAULT_ZOOM_LEVEL,
-    center: poems[0].latLng,
-  });
 
   poems.forEach((poem) => {
-    const marker = new google.maps.Marker({
-      position: poem.latLng,
-      title: poem.title,
-      map,
-    });
-    const contentHtml = poem.poem.replace(/\n/g, '<br/>');
-    let content = `<b>${poem.title}</b><br/><br/>
-            ${contentHtml}<br/>`;
-    if (poem.author) {
-      content = `${content}<br/>
-        -- ${poem.author}`;
-    }
-    const infoWindow = new google.maps.InfoWindow({
-      content,
-    });
-    marker.addListener('click', () => {
-      infoWindow.open(map, marker);
+    geocoder.geocode({ address: poem.address }, (results, status) => {
+      if (status !== 'OK') {
+        console.log(`unable to geocode ${poem.address}`);
+      }
+      poem.location = results[0].geometry.location;
+      addToMap(poem);
     });
   });
-
 }
 
 function handleSheetsResponse(data) {
@@ -58,14 +69,11 @@ function handleSheetsResponse(data) {
     console.log('no poems returned');
   }
   const poems = data.feed.entry.map((row) => {
-    const url = row[URL_KEY].$t;
-    const latLng = extractLatLngFromGoogleMapsUrl(url);
     const poem = {
       title: row[TITLE_KEY].$t,
       poem: row[POEM_KEY].$t,
       author: row[AUTHOR_KEY].$t,
-      url,
-      latLng,
+      address: row[ADDRESS_KEY].$t,
     };
     return poem;
   });
@@ -74,6 +82,10 @@ function handleSheetsResponse(data) {
 
 function initMap() {
   isMapInitialized = true;
+  geocoder = new google.maps.Geocoder();
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: DEFAULT_ZOOM_LEVEL,
+  });
 }
 
 $.getJSON(`https://spreadsheets.google.com/feeds/list/${GOOGLE_SHEET_ID}/1/public/values?alt=json-in-script&callback=?`, handleSheetsResponse);
